6268f04eb8ce4fa99e20851968a3c43a525e1719
Muhammad Awais Khan <107134941+Awais007khan@users.noreply.github.com>
2026-04-05 18:42:41 +0500

    import products from products.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -33,7 +33,9 @@
 }
 ];
 */
-import {cart} from '../data/cart.js'
+import {cart} from '../data/cart.js';
+import {products} from '../data/products.js';
+
 let productsHTML = '';
 
 products.forEach((product)=>{
