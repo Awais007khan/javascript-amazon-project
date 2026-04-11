c741f138cfea6c15b28a983c36062e679ed51d15
Muhammad Awais Khan <107134941+Awais007khan@users.noreply.github.com>
2026-04-11 10:46:49 +0500

    Create automated tests for formatCurrency

---
+++ tests/moneyTest.js
@@ -0,0 +1,27 @@
+import {formatCurrency} from '../scripts/utils/money.js'
+
+ // Basic Test Cases
+ console.log('test suite: formatCurrency');
+ console.log('Converts cents into dollars')
+if (formatCurrency(2095) === '20.95') {
+    console.log('passed');  
+} else{
+    console.log('failed');
+}
+// we`re using testing method to test diff test cases for our code to work properly
+
+// Edge Cases - test with values that are tricky
+console.log('works with 0');
+if(formatCurrency(0) === '0.00') {
+    console.log('passed again');
+
+} else{
+    console.log('failed');
+
+}
+console.log('rounds up to the nearest cent');
+if(formatCurrency(2000.5) === '20.01') {
+    console.log('Passed!');
+} else{
+    console.log('failed!');
+}
